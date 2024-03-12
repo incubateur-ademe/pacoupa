a7ca859d72f35fddeafbd75a8fc59e69680b9c0b
pom421 <mauguet.po@gmail.com>
2024-03-12 13:15:09 +0100

    feat: add funky underline text component

--- src/app/_landing/explication-pac-zone.tsx
+++ src/app/_landing/explication-pac-zone.tsx
@@ -1,6 +1,7 @@
 import { fr } from "@codegouvfr/react-dsfr";
 
 import { HesitationImage } from "@/components/img/HesitationImage";
+import { UnderlineText } from "@/components/UnderlineText";
 import { Box, Container, Grid, GridCol } from "@/dsfr";
 import { H2 } from "@/dsfr/base/typography";
 
@@ -20,9 +21,9 @@ export const ExplicationPacZone = () => {
               <H2>PAC, RCU: c'est quoi ?</H2>
               <p>Vous hésitez entre une chaudière à gaz ou des radiateurs électriques ?</p>
               <p>
-                Et pourquoi pas une <u>Pompe À Chaleur</u>
+                Et pourquoi pas une <UnderlineText>pompe à chaleur</UnderlineText> ?
                 <br />
-                ou un raccordement au <u>Réseau de Chaleur Urbain</u> ?
+                Ou un raccordement au <UnderlineText>réseau de chaleur urbain</UnderlineText> ?
               </p>
             </Box>
           </GridCol>
