803d2c4dcc5e280c17ee75980812a041f71dfd7f
pom421 <mauguet.po@gmail.com>
2024-03-18 16:04:07 +0100

    feat: add tooltip

--- src/app/_landing/explication-pac-zone.tsx
+++ src/app/_landing/explication-pac-zone.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 import { fr } from "@codegouvfr/react-dsfr";
+import { Tooltip } from "react-tooltip";
 
 import { HesitationImage } from "@/components/img/HesitationImage";
 import { UnderlineText } from "@/components/UnderlineText";
@@ -22,19 +25,33 @@ export const ExplicationPacZone = () => {
               <p>Vous hésitez entre une chaudière à gaz ou des radiateurs électriques ?</p>
               <p className={styles.questions}>
                 Et pourquoi pas une{" "}
-                <strong>
-                  <UnderlineText>pompe à chaleur</UnderlineText> ?
+                <strong className="pac">
+                  <UnderlineText>pompe à chaleur</UnderlineText>{" "}
+                  <span className={fr.cx("fr-icon--sm", "ri-information-line")} aria-hidden="true"></span> ?
                 </strong>
                 <br />
                 Ou un raccordement au{" "}
-                <strong>
-                  <UnderlineText>réseau de chaleur urbain</UnderlineText> ?
+                <strong className="rcu">
+                  <UnderlineText>réseau de chaleur urbain</UnderlineText>{" "}
+                  <span className={fr.cx("fr-icon--sm", "ri-information-line")} aria-hidden="true"></span> ?
                 </strong>
               </p>
             </Box>
           </GridCol>
         </Grid>
       </Container>
+
+      <Tooltip anchorSelect=".pac" place="top">
+        Une pompe à chaleur est un dispositif thermodynamique capable de transférer la chaleur <br />
+        d'un endroit à un autre, généralement utilisé pour chauffer ou refroidir un espace en exploitant <br />
+        les différences de température entre deux milieux.
+      </Tooltip>
+
+      <Tooltip anchorSelect=".rcu" place="top">
+        Le réseau de chaleur urbain est un système de distribution centralisé de chaleur produite <br />
+        à partir de sources diverses telles que la biomasse, la géothermie, etc.. alimentant plusieurs <br />
+        bâtiments dans une zone urbaine pour le chauffage et parfois le refroidissement.
+      </Tooltip>
     </>
   );
 };
