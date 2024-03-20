6be7da5859dd0069c09d7ab0bd31f5e97537211e
pom421 <mauguet.po@gmail.com>
2024-03-20 11:39:07 +0100

    feat: test with dynamic route

---
+++ src/app/simu/[CH]/page.tsx
@@ -0,0 +1,24 @@
+import { turso } from "@/lib/turso";
+
+const sql = (
+  CH: string = "IND",
+) => `select solution, espace_exterieur, environnement, toiture_terrasse, temperature, nb_lgts, niveau_renovation, difficulte, impact_travaux_coll, impact_travaux_indiv, cout, type_solution, usage_CH, usage_ECS, usage_FR, ordre_solution \
+  from simu1 where CH = '${CH}' and ECS = 'IND' and emetteur = 'Hydraulique' and temperature = 'Superieur à 60°C' and (niveau_renovation = 'Recent ou renove' or niveau_renovation = 'NA') and (nb_lgts = '> 15' or nb_lgts = 'NA') order by ordre_solution`;
+
+const Page = async ({ params }: { params: { CH: string } }) => {
+  // const { rows } = await turso.execute("SELECT * FROM simu1");
+  const { rows } = await turso.execute(sql(params.CH));
+
+  return (
+    <>
+      <strong>Nb rows: {rows.length}</strong>
+      <ul>
+        {rows.map((row, index) => (
+          <li key={index}>{JSON.stringify(row)}</li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default Page;
