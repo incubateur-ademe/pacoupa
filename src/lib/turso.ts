de247bedce3578c14dfc1c8759e9a5b5f7c68f67
pom421 <mauguet.po@gmail.com>
2024-03-20 12:32:27 +0100

    feat: add turso url

--- src/lib/turso.ts
+++ src/lib/turso.ts
@@ -2,7 +2,7 @@ import { createClient } from "@libsql/client";
 // import { createClient } from "@libsql/client/web";
 
 export const turso = createClient({
-  url: "file:pacoupa.db",
-  // url: process.env.TURSO_DATABASE_URL!,
-  // authToken: process.env.TURSO_AUTH_TOKEN,
+  // url: "file:pacoupa.db",
+  url: process.env.TURSO_DATABASE_URL!,
+  authToken: process.env.TURSO_AUTH_TOKEN,
 });
