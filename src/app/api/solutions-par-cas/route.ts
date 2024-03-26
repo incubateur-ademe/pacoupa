29485e1a0aaaed79d402e3f51879261b27f225d4
pom421 <mauguet.po@gmail.com>
2024-03-26 12:23:49 +0100

    feat: improve sql query for NA fields

--- src/app/api/solutions-par-cas/route.ts
+++ src/app/api/solutions-par-cas/route.ts
@@ -1,5 +1,5 @@
-import { caracteristiques } from "drizzle/schema";
-import { type Column, eq, sql } from "drizzle-orm";
+import { caracteristiques, solutions, solutionsParCas } from "drizzle/schema";
+import { eq, or, sql } from "drizzle-orm";
 import { createSelectSchema } from "drizzle-zod";
 import { z } from "zod";
 
@@ -7,23 +7,45 @@ import { db } from "@/lib/drizzle";
 
 export const dynamic = "force-dynamic"; // defaults to auto
 
-const OuiNonSchema = z.enum(["Oui", "Non"]).optional();
+const OuiNonSchema = z.enum(["Oui", "Non"]);
 
 const selectCaracteristiquesSchema = createSelectSchema(caracteristiques, {
   id: schema => schema.id.optional(),
-  ch: z.enum(["IND", "COL"]).optional(),
-  ecs: z.enum(["IND", "COL"]).optional(),
-  emetteur: z.enum(["Hydraulique", "Electrique"]).optional(),
-  envContraint: z.enum(["Terrain disponible", "Contraint"]).optional(),
+  ch: z.enum(["IND", "COL", "ind", "col"]),
+  ecs: z.enum(["IND", "COL"]),
+  emetteur: z.enum(["Hydraulique", "Electrique"]),
+  envContraint: z.enum(["Terrain disponible", "Contraint"]),
   espaceExterieur: OuiNonSchema,
-  toitureTerrasse: z.enum(["Sans TT", "Toiture T"]).optional(),
-  nbLgts: z.enum(["< 15", "> 15"]).optional(),
-  niveauRenovation: z.enum(["Recent ou renove"]).optional(),
-  temperature: schema => schema.temperature.optional(), // TODO: changer les valeurs dans le google sheet
+  toitureTerrasse: z.enum(["Sans TT", "Toiture T"]),
+  nbLgts: z.enum(["< 15", "> 15"]),
+  niveauRenovation: z.enum(["Recent ou renove"]).optional(), // non rempli si non récent et non rénové
+  temperature: z.enum(["Inferieur à 40°C", "Superieur à 60°C", "Inferieur à 60°C", "Entre 40°C et 60°C"]).optional(), // non rempli si émetteur est électrique
 });
 
-const buildCondition = (column: Column, value: number | string) => {
-  return sql`${column} = ${value}`;
+const NAFields = ["envContraint", "espaceExterieur", "toitureTerrasse", "nbLgts", "niveauRenovation", "temperature"];
+
+/**
+ * Build a SQL condition from the filtersb
+ */
+const buildConditions = (filters: z.infer<typeof selectCaracteristiquesSchema>) => {
+  const keys = Object.keys(filters);
+
+  const sqlChunks = keys.map(key => {
+    if (NAFields.includes(key)) {
+      const firstPart = sql`${caracteristiques[key as keyof typeof caracteristiques]} = ${
+        filters[key as keyof typeof filters]
+      }`;
+      const secondPart = sql`${caracteristiques[key as keyof typeof caracteristiques]} = 'NA'`;
+
+      return sql`${or(firstPart, secondPart)}`;
+    } else {
+      return sql`upper(${caracteristiques[key as keyof typeof caracteristiques]}) = upper(${
+        filters[key as keyof typeof filters]
+      })`;
+    }
+  });
+
+  return sql`${sql.join(sqlChunks, sql.raw(" AND "))}`;
 };
 
 export async function POST(request: Request) {
@@ -33,28 +55,15 @@ export async function POST(request: Request) {
     return Response.json({ error: res.error });
   }
 
-  console.log("body", JSON.stringify(res, null, 2));
+  // console.log("body", JSON.stringify(res, null, 2));
 
-  let rows;
+  const rows = await db
+    .select()
+    .from(caracteristiques)
+    .innerJoin(solutionsParCas, eq(caracteristiques.id, solutionsParCas.caracteristiquesId))
+    .innerJoin(solutions, eq(solutionsParCas.idSolution, solutions.id))
+    .where(buildConditions(res.data))
+    .all();
 
-  const conditions = Object.keys(res.data)
-    .map(key => {
-      if (res.data[key as keyof typeof res.data]) {
-        return buildCondition(key as unknown as Column, res.data[key as keyof typeof res.data] as string);
-      } else {
-        return null;
-      }
-    })
-    .filter(Boolean)
-    .join(" AND ");
-
-  console.log("conditions", conditions);
-
-  if (res.data.ch) {
-    rows = await db.select().from(caracteristiques).where(eq(caracteristiques.ch, res.data.ch)).all();
-  } else {
-    rows = await db.select().from(caracteristiques).all();
-  }
-
-  return Response.json({ data: rows, nbRows: rows.length });
+  return Response.json({ nbRows: rows.length, data: rows });
 }
