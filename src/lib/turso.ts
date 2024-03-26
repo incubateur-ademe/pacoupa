080e79cea4cdde240527d589601966df9c30557c
pom421 <mauguet.po@gmail.com>
2024-03-26 17:42:53 +0100

    fix: yarn build doesn't work without a placeholder for TURSO_DATABASE_URL

--- src/lib/turso.ts
+++
@@ -1,8 +0,0 @@
-import { createClient } from "@libsql/client";
-// import { createClient } from "@libsql/client/web";
-
-export const turso = createClient({
-  // url: "file:pacoupa.db",
-  url: process.env.TURSO_DATABASE_URL!,
-  authToken: process.env.TURSO_AUTH_TOKEN,
-});
