c12e23294341473216dade05639cc6abbcf09a3f
pom421 <mauguet.po@gmail.com>
2024-04-03 10:59:31 +0200

    doc: comment

--- src/components/Wizard.tsx
+++ src/components/Wizard.tsx
@@ -5,6 +5,10 @@ import { Wizard as WizardBase } from "react-use-wizard";
 
 type Props = Parameters<typeof WizardBase>[0];
 
+/**
+ * Wrapper around react-use-wizard/Wizard to be used in server component architecture.
+ *
+ */
 export const Wizard = (props: PropsWithChildren<Props>) => {
   return (
     <>
