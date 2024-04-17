2f2a12340a8e4a4ff9cd656d026c34cc70406124
pom421 <mauguet.po@gmail.com>
2024-04-17 12:03:47 +0200

    feat: Oui is clicked by default for DSFR issue with this component on a11y

--- src/app/simulation/etapes/Step6/index.tsx
+++ src/app/simulation/etapes/Step6/index.tsx
@@ -43,7 +43,7 @@ export const Step6 = () => {
 
   useEffect(() => {
     if (sessionStorageOK) {
-      setRadioState(initialState?.possedeEspacesExterieursPersonnels as (typeof OuiNonLabels)[number]);
+      setRadioState((initialState?.possedeEspacesExterieursCommuns as (typeof OuiNonLabels)[number]) ?? "Oui");
     }
   }, [sessionStorageOK, initialState]);
 
