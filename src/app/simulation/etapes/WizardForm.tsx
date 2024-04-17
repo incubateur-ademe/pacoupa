76f1499542f8c8c9def6fcb9fca4a6e4bde8ac47
Pierre-Olivier Mauguet <pierre-olivier.mauguet@beta.gouv.fr>
2024-04-17 16:48:05 +0200

    feat: refactor sessionstorage (#23)
    * feat: refactor with useSessionStorage hook

--- src/app/simulation/etapes/WizardForm.tsx
+++ src/app/simulation/etapes/WizardForm.tsx
@@ -1,11 +1,13 @@
 import { Base64 } from "js-base64";
 import { useRouter } from "next/navigation";
-import { useCallback, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useWizard } from "react-use-wizard";
-import { z, type ZodFormattedError } from "zod";
+import { type z, type ZodFormattedError } from "zod";
 
-import { store } from "@/lib/client/store";
+import { usePacoupaSessionStorage } from "@/lib/client/usePacoupaSessionStorage";
+import { createObjectFromFormData } from "@/lib/form-data";
 
+import { type SimulationSchema } from "../schema";
 import { ButtonsFunnel } from "./ButtonsFunnel";
 
 export type HandleFormResult =
@@ -17,49 +19,11 @@ export type HandleFormResult =
       success: true;
     };
 
-/**
- * Store the FormData in the session storage or return an error if the form is invalid.
- *
- * @param schema zod schema for the current form
- * @param formAction function to execute if the form is valid
- */
-export const handleForm =
-  (schema: z.AnyZodObject, formAction: (handleResult: HandleFormResult) => void) =>
-  (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-
-    const data = new FormData(event.currentTarget);
-
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-    const keys = Object.keys(schema.shape) as unknown as Array<keyof z.infer<typeof schema>>;
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-    const arrayKeys = keys.filter(key => schema.shape[key] instanceof z.ZodArray);
-
-    const values = Object.fromEntries(
-      Array.from(data.keys()).map(key => [key, arrayKeys.includes(key) ? data.getAll(key) : data.get(key)]),
-    );
-
-    console.debug("Debug values", JSON.stringify(values));
-
-    const validation = schema.safeParse(values);
-
-    let result: HandleFormResult;
-
-    if (validation.success) {
-      store.update(validation.data);
-      result = { success: true };
-    } else {
-      console.log("error", validation.error);
-
-      result = { success: false, errors: validation.error.format() };
-    }
-
-    formAction(result);
-  };
-
 type Props = {
   render: ({
     errors,
+    store,
+    resetStore,
   }: {
     errors:
       | z.ZodFormattedError<
@@ -69,6 +33,8 @@ type Props = {
           string
         >
       | undefined;
+    resetStore: () => void;
+    store: Partial<SimulationSchema>;
   }) => JSX.Element;
   schema: z.AnyZodObject;
 };
@@ -77,35 +43,46 @@ export const WizardForm = ({ schema, render }: Props) => {
   const { nextStep, isLastStep } = useWizard();
   const [errors, setErrors] = useState<ZodFormattedError<{ [x: string]: unknown }, string>>();
   const router = useRouter();
+  const { store, setStore, resetStore } = usePacoupaSessionStorage();
 
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
 
-  const formAction = useCallback(
-    (result: HandleFormResult) => {
-      if (result.success) {
-        const encoded = Base64.encode(JSON.stringify(store.get()));
+  /**
+   * Store the FormData in the session storage or return an error if the form is invalid.
+   *
+   * @param schema zod schema for the current form
+   * @param formAction function to execute if the form is valid
+   */
+  const handleForm = (schema: z.AnyZodObject) => (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
 
-        if (isLastStep) {
-          return router.push(`/simulation/resultat?hash=${encoded}`);
-        }
+    const formData = new FormData(event.currentTarget);
+    const validation = schema.safeParse(createObjectFromFormData(schema)(formData));
 
-        nextStep().catch(() => {
-          console.error("erreur dans la navigation");
-        });
-      } else {
-        console.log("error", result.errors);
-        setErrors(result.errors);
+    if (validation.success) {
+      const nextStore = { ...store, ...validation.data } as SimulationSchema;
+      setStore(nextStore);
+
+      if (isLastStep) {
+        const encoded = Base64.encode(JSON.stringify(nextStore));
+        return router.push(`/simulation/resultat?hash=${encoded}`);
       }
-    },
-    [nextStep, setErrors, isLastStep, router],
-  );
+
+      nextStep().catch(() => {
+        console.error("erreur dans la navigation");
+      });
+    } else {
+      console.log("error", validation.error);
+      setErrors(validation.error.format());
+    }
+  };
 
   return (
     <>
-      <form onSubmit={handleForm(schema, formAction)} noValidate>
-        {render({ errors })}
+      <form onSubmit={handleForm(schema)} noValidate>
+        {render({ errors, store, resetStore })}
 
         <ButtonsFunnel />
       </form>
