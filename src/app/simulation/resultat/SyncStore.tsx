7c280bb07ede7fa632b70123e4a4c56780cd0649
Pierre-Olivier Mauguet <pierre-olivier.mauguet@beta.gouv.fr>
2024-04-22 16:46:33 +0200

    fix: sync sessionstorage (#26)
    * fix: layout of simulation's steps
    
    * fix: syns session storage on resultat page

---
+++ src/app/simulation/resultat/SyncStore.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { Base64 } from "js-base64";
+import { type PropsWithChildren, useEffect } from "react";
+
+import { usePacoupaSessionStorage } from "@/lib/client/usePacoupaSessionStorage";
+
+import { type SimulationSchema } from "../schema";
+
+type Props = {
+  hash: string;
+};
+
+/**
+ * Client component to store the simulation as a hash in the session storage.
+ */
+export const SyncStore = ({ hash }: PropsWithChildren<Props>) => {
+  const { setStore } = usePacoupaSessionStorage();
+
+  useEffect(() => {
+    setStore(JSON.parse(Base64.decode(hash)) as SimulationSchema);
+  }, [hash, setStore]);
+
+  return null;
+};
