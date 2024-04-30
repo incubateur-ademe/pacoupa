30da96c6678ca6fb65c2c15ad370d661c25b6317
pom421 <mauguet.po@gmail.com>
2024-04-30 21:14:15 +0200

    feat: Badge for now

--- src/app/simulation/resultat/Recommandation.tsx
+++ src/app/simulation/resultat/Recommandation.tsx
@@ -1,5 +1,6 @@
 import { fr } from "@codegouvfr/react-dsfr";
 import { cx } from "@codegouvfr/react-dsfr/tools/cx";
+import { Badge } from "@mui/material";
 
 import { ChauffageImage } from "@/components/img/usages/ChauffageImage";
 import { ClimatisationImage } from "@/components/img/usages/ClimatisationImage";
@@ -13,30 +14,42 @@ export const Recommandation = ({ solution }: { solution: GetSolutionsParCriteres
   return (
     <Box className={cx("flex", "justify-between", "gap-4")}>
       <div className={cx("flex", "flex-col", "items-center")}>
-        <ChauffageImage
-          enabled={usageCH === "Oui"}
-          alt={
-            usageCH === "Oui"
-              ? "Solution permettant le chauffage"
-              : usageCH === "Non"
-                ? "Solution ne permettant pas le chauffage"
-                : "Solution permettant potentiellement le chauffage"
-          }
-        />
+        <Badge
+          variant="dot"
+          color={usageCH === "Oui" ? "success" : usageCH === "Non" ? "error" : "info"}
+          anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+        >
+          <ChauffageImage
+            enabled={usageCH === "Oui"}
+            alt={
+              usageCH === "Oui"
+                ? "Solution permettant le chauffage"
+                : usageCH === "Non"
+                  ? "Solution ne permettant pas le chauffage"
+                  : "Solution permettant potentiellement le chauffage"
+            }
+          />
+        </Badge>
         <span className={fr.cx("fr-text--xs", "fr-mt-1w")}>Chauffage</span>
       </div>
 
       <div className={cx("flex", "flex-col", "items-center")}>
-        <EcsImage
-          enabled={usageECS === "Oui"}
-          alt={
-            usageECS === "Oui"
-              ? "Solution permettant l'eau chaude sanitaire"
-              : usageECS === "Non"
-                ? "Solution ne permettant pas l'eau chaude sanitaire"
-                : "Solution permettant potentiellement l'eau chaude sanitaire"
-          }
-        />
+        <Badge
+          variant="dot"
+          color={usageECS === "Oui" ? "success" : usageECS === "Non" ? "error" : "info"}
+          anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+        >
+          <EcsImage
+            enabled={usageECS === "Oui"}
+            alt={
+              usageECS === "Oui"
+                ? "Solution permettant l'eau chaude sanitaire"
+                : usageECS === "Non"
+                  ? "Solution ne permettant pas l'eau chaude sanitaire"
+                  : "Solution permettant potentiellement l'eau chaude sanitaire"
+            }
+          />
+        </Badge>
         <span className={cx(fr.cx("fr-text--xs", "fr-mt-1w"), "text-center")}>
           Eau chaude
           <br /> sanitaire
@@ -44,16 +57,22 @@ export const Recommandation = ({ solution }: { solution: GetSolutionsParCriteres
       </div>
 
       <div className={cx("flex", "flex-col", "items-center")}>
-        <ClimatisationImage
-          enabled={usageFr === "Oui"}
-          alt={
-            usageFr === "Oui"
-              ? "Solution permettant la climatisation"
-              : usageFr === "Non"
-                ? "Solution ne permettant pas la climatisation"
-                : "Solution permettant potentiellement la climatisation"
-          }
-        />
+        <Badge
+          variant="dot"
+          color={usageFr === "Oui" ? "success" : usageFr === "Non" ? "error" : "info"}
+          anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+        >
+          <ClimatisationImage
+            enabled={usageFr === "Oui"}
+            alt={
+              usageFr === "Oui"
+                ? "Solution permettant la climatisation"
+                : usageFr === "Non"
+                  ? "Solution ne permettant pas la climatisation"
+                  : "Solution permettant potentiellement la climatisation"
+            }
+          />
+        </Badge>
         <span className={fr.cx("fr-text--xs", "fr-mt-1w")}>Climatisation</span>
       </div>
     </Box>
