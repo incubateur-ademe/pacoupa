a6c469f6095d79727fc6499014b128a59d25dc7f
pom421 <mauguet.po@gmail.com>
2024-05-15 15:55:07 +0200

    feat: customize Matomo config

--- src/components/utils/Matomo.tsx
+++ src/components/utils/Matomo.tsx
@@ -6,9 +6,11 @@ import { useEffect, useState } from "react";
 
 import { config } from "@/config";
 
-import { useConsent } from "../../consentManagement";
-
 export type MatomoProps = Pick<typeof config, "env"> & { nonce?: string };
+
+// Set optin tracking. This seemms reasonable since Matomo is configured with no cookies and it stores no personnal data.
+// If some privacy issues are found, we'll use useConsent as commented below and add a consent banner.
+const matomoConsent = true;
 
 /**
  * Handle Matomo init and consent.
@@ -18,67 +20,69 @@ export type MatomoProps = Pick<typeof config, "env"> & { nonce?: string };
 export const Matomo = ({ env, nonce }: MatomoProps) => {
   const pathname = usePathname();
   const searchParams = useSearchParams();
-  const { finalityConsent } = useConsent();
-  const matomoConsent = finalityConsent?.matomo;
-  const [inited, setInited] = useState(false);
+  // const { finalityConsent } = useConsent();
+  // const matomoConsent = finalityConsent?.matomo;
+  const [initialized, setInitialized] = useState(false);
   const [previousPath, setPreviousPath] = useState("");
-
-  console.log("env pour matomo", env);
-  console.log("config.matomo", config.matomo);
 
   useEffect(() => {
     // if (env === "dev") {
     //   return;
     // }
 
-    if (!inited) {
+    if (!initialized) {
       init({
         ...config.matomo,
         disableCookies: true,
         nonce,
         onInitialization: () => {
-          push(["optUserOut"]);
-          push(["requireCookieConsent"]);
+          // Tracking by default.
+          // push(["optUserOut"]);
+
+          // Tracking without cookies.
+          push(["disableCookies"]);
+          // push(["requireCookieConsent"]);
+
+          // People may have configured their browser with DoNotTrack feature. Their preferences will be respected.
+          push(["setDoNotTrack", "true"]);
+
+          // Time on page tracking enabled.
           push(["enableHeartBeatTimer"]);
           push(["disableQueueRequest"]);
           push(["disablePerformanceTracking"]);
         },
       });
-      setInited(true);
+      setInitialized(true);
     }
 
     // if (matomoConsent) {
-    if (!matomoConsent) {
-      console.debug("Activation des cookies Matomo.");
-      push(["forgetUserOptOut"]);
-      push(["rememberCookieConsentGiven"]);
-    } else {
-      console.debug("Désactivation des cookies Matomo.");
-      push(["optUserOut"]);
-      push(["forgetCookieConsentGiven"]);
-    }
+    //   console.debug("Activation des cookies Matomo.");
+    //   push(["forgetUserOptOut"]);
+    //   push(["rememberCookieConsentGiven"]);
+    // } else {
+    //   console.debug("Désactivation des cookies Matomo.");
+    //   push(["optUserOut"]);
+    //   push(["forgetCookieConsentGiven"]);
+    // }
     // eslint-disable-next-line react-hooks/exhaustive-deps -- don't listen on inited
-  }, [env, matomoConsent]);
+  }, [env]);
 
-  /* The @socialgouv/matomo-next does not work with next 13 */
+  /* The @socialgouv/matomo-next does not work with next 13, so we need to handle by ourselves */
   useEffect(() => {
     // if (!pathname || !matomoConsent || env === "dev") {
-    //   return;
-    // }
+    if (!pathname || !matomoConsent) {
+      return;
+    }
 
     if (!previousPath) {
       return setPreviousPath(pathname);
     }
 
     push(["setReferrerUrl", `${previousPath}`]);
-    push(["setCustomUrl", pathname]);
-    push(["deleteCustomVariables", "page"]);
-    push(["deleteCustomVariable", "page"]);
     setPreviousPath(pathname);
     // In order to ensure that the page title had been updated,
     // we delayed pushing the tracking to the next tick.
     setTimeout(() => {
-      push(["setDocumentTitle", document.title]);
       if (pathname.startsWith("/recherche")) {
         push(["trackSiteSearch", searchParams?.get("keyword") ?? searchParams?.get("query") ?? ""]);
       } else {
@@ -86,13 +90,7 @@ export const Matomo = ({ env, nonce }: MatomoProps) => {
         push(["trackPageView"]);
       }
     });
-    /**
-     * This is because we don't want to track previousPath
-     * could be a if (previousPath === pathname) return; instead
-     * But be sure to not send the tracking twice
-     */
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [matomoConsent, pathname, searchParams]);
+  }, [env, pathname, previousPath, searchParams]);
 
   return <></>;
 };
