0f804a226e892e91775f59f7ed475c3be30924a6
Emmanuel Dissoubray <edissoubray@users.noreply.github.com>
2024-05-16 16:30:46 +0200

    Update 26.ts
    Wording

--- content/solutions/26.ts
+++ content/solutions/26.ts
@@ -11,31 +11,92 @@ export const solution = {
   usageFr: "Possible",
   environnement: {
     note: "C",
-    text: [],
+    text: [
+      {
+        titre: "Combiner PAC et chaudière",
+        contenu:
+          "La solution hybride permet d’avoir une pompe à chaleur de petite puissance, qui est complétée en cas de besoin par une chaudière. Cela peut être très pertinent si votre chaudière est encore pleinement opérationnelle, ou si votre bâtiment impose des contraintes trop fortes pour fonctionner à 100% avec une pompe à chaleur.",
+      },
+      {
+        titre: "Bénéfice variable",
+        contenu:
+          "Le bénéfice environnemental dépend du taux d’hybridation : c’est la proportion d'énergie fournie par la pompe à chaleur par rapport à celle fournie par la chaudière à gaz. Plus le taux de couverture de la pompe à chaleur est important (c’est-à-dire que l’énergie produite vient de la PAC), meilleur sera le bénéfice environnemental.",
+      },
+    ],
   },
   cout: {
     note: "dynamic",
   },
   difficulte: {
     note: "dynamic",
-    text: [],
+    text: [
+      {
+        "titre": "Structure",
+        "contenu": "En cas de pose en toiture-terrasse, anticipez l’impact des PAC sur la capacité structurelle de la toiture."
+      },
+      {
+        "titre": "Raccordement électrique",
+        "contenu": "Anticipez l’impact sur la puissance de raccordement électrique."
+      },
+      {
+        "titre": "Réseaux hydrauliques",
+        "contenu": "Nettoyage et isolation des réseaux chauffage à prévoir pour optimiser la performance de l’installation."
+      }
+    ],
   },
   travauxCollectif: {
     note: "dynamic",
     image: "06-16-26 - Hybride PAC gainée + chaudiere - parties communes.png",
-    text: [],
+    text: [
+      {
+        titre: "Déroulement des travaux",
+        contenu:
+          "Travaux rapides dans des parties communes peu fréquentées.",
+      },
+    ],
   },
   travauxIndividuel: {
     note: "dynamic",
-    text: [],
+    text: [
+      {
+        titre: "Emprise logement",
+        contenu: "Aucune.",
+      },
+      {
+        titre: "Déroulement des travaux",
+        contenu: "Pas de travaux dans les appartements.",
+      },
+    ],
   },
   acoustique: {
     note: "C",
-    text: [],
+    text: [
+      {
+        titre: "Volume sonore",
+        contenu:
+          "De 45 à 65dB (bouche en façade) / de 50 à 65dB (unité intérieure)",
+      },
+      {
+        titre: "Acoustique",
+        contenu:
+          "Le bruit d’une PAC est couvert par les bruits ambiants la journée mais peut être gênant la nuit si les bouches en façade sont trop proches des chambres. Il existe des solutions pour diminuer l’impact sonore des bouches en façade.",
+      },
+    ],
   },
   espaceExterieur: {
     note: "A",
-    text: [],
+    text: [
+      {
+        titre: "Impact extérieur",
+        contenu:
+          "Il n’y a pas d’unité extérieure, simplement une bouche en façade. La pompe à chaleur est située dans le local technique et est connectée à l’air extérieur par des gaines. Il faut anticiper l’intégration architecturale des bouches en façade.",
+      },
+      {
+        titre: "PLU",
+        contenu:
+          "Vérifiez que l’intégration architecturales des bouches en façade est compatible avec le PLU.",
+      },
+    ],
   },
   maturite: {
     note: "D",
