dbed2349c2cf7f86f5afbf8d03445bf5f8df3fdd
Emmanuel Dissoubray <edissoubray@users.noreply.github.com>
2024-05-16 15:36:37 +0200

    Update 08.ts
    wording

--- content/solutions/08.ts
+++ content/solutions/08.ts
@@ -11,16 +11,17 @@ export const solution = {
   usageEcs: "Oui",
   usageFr: "Non",
   environnement: {
-    note: "B",
+    note: "A",
     text: [
       {
         titre: "Consommation d’énergie",
         contenu:
-          "La pompe à chaleur utilise une énergie décarbonée (l’électricité). Son rendement est, en moyenne, trois fois supérieur à celui d’un radiateur électrique ou d’une chaudière.",
+          "La pompe à chaleur utilise une énergie décarbonée (l’électricité). Le rendement d'une pompe à chaleur géothermique est, en moyenne, cinq fois supérieur à celui d’un radiateur électrique ou d’une chaudière.",
       },
       {
         titre: "Entretien",
-        contenu: "Au même titre qu’une chaudière, il est important de prévoir une maintenance régulière des appareils.",
+        contenu:
+          "Au même titre qu’une chaudière, il est important de prévoir une maintenance régulière des appareils.",
       },
     ],
   },
@@ -30,24 +31,65 @@ export const solution = {
   difficulte: {
     note: "dynamic",
     image: "08-13-23 - PAC geothermique - facilite installation.png",
-    text: [],
+    text: [
+      {
+        titre: "Sonde géothermique",
+        contenu:
+          "Une PAC eau-eau capte des calories sur des sondes géothermiques verticales ou sur l’eau d’une nappe phréatique. Il faut donc prévoir le forage pour la sonde et/ou l’installation d’une pompe pour la nappe phréatique.",
+      },
+      {
+        titre: "Raccordement électrique",
+        contenu: "Anticipez l’impact sur la puissance de raccordement électrique.",
+      },
+      {
+        titre: "Réseaux hydrauliques",
+        contenu:
+          "Nettoyage et isolation des réseaux chauffage et ECS à prévoir pour optimiser la performance de l’installation.",
+      },
+    ],
   },
   travauxCollectif: {
     note: "dynamic",
     image: "08-13-23 - PAC geothermique - parties communes.png",
-    text: [],
+    text: [
+      {
+        titre: "Déroulement des travaux",
+        contenu:
+          "Travaux importants dans les parties extérieures comme le jardin et/ou la nappe phréatique le temps de l’installation.",
+      },
+    ],
   },
   travauxIndividuel: {
     note: "dynamic",
-    text: [],
+    text: [
+      {
+        titre: "Emprise logement",
+        contenu: "Aucune.",
+      },
+      {
+        titre: "Déroulement des travaux",
+        contenu: "Pas de travaux dans les appartements.",
+      },
+    ],
   },
   acoustique: {
     note: "A",
-    text: [],
+    text: [
+      {
+        titre: "Acoustique",
+        contenu: "Aucune nuisance sonore.",
+      },
+    ],
   },
   espaceExterieur: {
-    note: "B",
-    text: [],
+    note: "A",
+    text: [
+      {
+        titre: "Sonde géothermique",
+        contenu:
+          "Une PAC eau-eau capte des calories sur des sondes géothermiques verticales ou sur l’eau d’une nappe phréatique. Une fois l’installation faite, il n’y a plus d’impact visuel.",
+      },
+    ],
   },
   maturite: {
     note: "A",
