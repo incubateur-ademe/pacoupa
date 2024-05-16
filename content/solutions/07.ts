75c60f8c99189ea4529a200849ff3ed2e96f2ce3
Emmanuel Dissoubray <edissoubray@users.noreply.github.com>
2024-05-16 15:32:52 +0200

    Update 07.ts
    Wording

--- content/solutions/07.ts
+++ content/solutions/07.ts
@@ -20,7 +20,8 @@ export const solution = {
       },
       {
         titre: "Entretien",
-        contenu: "Au même titre qu’une chaudière, il est important de prévoir une maintenance régulière des appareils.",
+        contenu:
+          "Au même titre qu’une chaudière, il est important de prévoir une maintenance régulière des appareils.",
       },
     ],
   },
@@ -29,24 +30,79 @@ export const solution = {
   },
   difficulte: {
     note: "dynamic",
-    text: [],
+    text: [
+      {
+        titre: "Structure",
+        contenu:
+          "La pose étant en toiture-terrasse, anticipez l’impact des PAC sur la capacité structurelle de la toiture.",
+      },
+      {
+        titre: "Raccordement électrique",
+        contenu: "Anticipez l’impact sur la puissance de raccordement électrique.",
+      },
+      {
+        titre: "Réseaux hydrauliques",
+        contenu:
+          "Nettoyage et isolation des réseaux chauffage et ECS à prévoir pour optimiser la performance de l’installation.",
+      },
+    ],
   },
   travauxCollectif: {
     note: "dynamic",
-    text: [],
+    text: [
+      {
+        titre: "Déroulement des travaux",
+        contenu: "Travaux rapides dans des parties communes peu fréquentées.",
+      },
+    ],
   },
   travauxIndividuel: {
     note: "dynamic",
-    text: [],
+    text: [
+      {
+        titre: "Emprise logement",
+        contenu: "Aucune.",
+      },
+      {
+        titre: "Déroulement des travaux",
+        contenu: "Pas de travaux dans les appartements.",
+      },
+    ],
   },
   acoustique: {
     note: "B",
-    text: [],
+    text: [
+      {
+        titre: "Volume sonore",
+        contenu: "de 45 à 65dB (unité extérieure) / de 50 à 65dB (unité intérieure)",
+      },
+      {
+        titre: "Acoustique",
+        contenu: "Le bruit d’une PAC est couvert par les bruits ambiants la journée mais peut être gênant la nuit si l’unité est trop proche des chambres. Il existe des solutions pour diminuer l’impact sonore des unités extérieures.",
+      },
+    ],
   },
   espaceExterieur: {
     note: "B",
     image: "07 - PAC sur air extrait - impact exterieur.png",
-    text: [],
+    text: [
+      {
+        titre: "Unité extérieure",
+        contenu: "Les PAC air / eau disposent d’une unité extérieure (toiture ou terrasse).",
+      },
+      {
+        titre: "Emprise des PAC",
+        contenu: "Assurez-vous que la place est suffisante en extérieur (voir la surface estimée).",
+      },
+      {
+        titre: "Structure",
+        contenu: "La pose étant en toiture-terrasse, anticipez l’impact des PAC sur la capacité structurelle de la toiture.",
+      },
+      {
+        titre: "PLU",
+        contenu: "Vérifiez que l'installation de PAC est compatible avec le PLU (notamment la hauteur si la PAC est en toiture).",
+      },
+    ],
   },
   maturite: {
     note: "E",
