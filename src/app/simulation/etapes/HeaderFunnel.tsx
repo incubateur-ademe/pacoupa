73808908c131cfef932548f48b99e7feda98388b
Pierre-Olivier Mauguet <pierre-olivier.mauguet@beta.gouv.fr>
2024-05-29 06:09:16 -0700

    feat: refactor stepper counter (#38)

--- src/app/simulation/etapes/HeaderFunnel.tsx
+++ src/app/simulation/etapes/HeaderFunnel.tsx
@@ -6,23 +6,51 @@ import { useWizard } from "react-use-wizard";
 
 import { Box } from "@/dsfr";
 
-const titles = ["Le bâtiment", "Le chauffage", "L'eau chaude"];
-const displayStepCount = 3;
+// Be careful: the DSFR doesn't allow to have more than 8 steps... So ensure that each phase has less than 8 steps.
+const phases = [
+  ["Le bâtiment", 6],
+  ["Le chauffage", 3],
+  ["L'eau chaude", 2],
+] as const;
 
+const findPhase = (step: number) => {
+  let phase = 0;
+
+  let currentStep = step;
+
+  for (let i = 0; i < phases.length; i++) {
+    if (currentStep >= phases[i][1]) {
+      currentStep = currentStep - phases[i][1];
+      phase++;
+    } else {
+      return {
+        phase: phases[phase],
+        currentStep,
+        nextTitle: phases[phase + 1]?.[0] ?? "",
+      };
+    }
+  }
+
+  throw new Error("Invalid step");
+};
+
+/*
+ * Note: we use the DSFR stepper in a unusual way. The DSFR stepper is limited to only 8 steps before the progression bar is not displayed anymore.
+ *
+ * So we use 3 phases to group the steps and display the progression bar.
+ * There is still a limitation, because the nextTitle is not displayed when it is the last step of a phase. So we can't
+ * use it for our case.
+ *
+ */
 export const HeaderFunnel = () => {
   const { activeStep } = useWizard();
 
-  // NB: the DSFR doesn't allow to have more than 8 steps...
-  const displayStep = activeStep < 6 ? 0 : activeStep < 9 ? 1 : 2;
+  const { phase, currentStep } = findPhase(activeStep);
 
   return (
     <Box className={fr.cx("fr-mt-4w")}>
-      <Stepper
-        currentStep={displayStep + 1}
-        stepCount={displayStepCount}
-        nextTitle={displayStep < displayStepCount ? titles[displayStep + 1] : ""}
-        title={titles[displayStep]}
-      />
+      {/* We can't use nextTitle because it is not shown when it is the last step of a phase... */}
+      <Stepper currentStep={currentStep + 1} stepCount={phase[1]} title={phase[0]} />
     </Box>
   );
 };
