07521b6b970a84fc1acb77e879618909bedb1e29
pom421 <mauguet.po@gmail.com>
2024-06-18 17:51:01 +0200

    feat: refactor colors with css var and tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
@@ -11,6 +14,23 @@ module.exports = {
       md: "48em",
       lg: "62em",
       xl: "78em",
+    },
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      black: colors.black,
+      white: colors.white,
+      green: {
+        900: "#183d2f", //     color: rgb(24 61 47 / var(--tw-text-opacity));
+        800: "#274430", //     color: rgb(39 68 48 / var(--tw-text-opacity));
+        700: "#304436", //     color: rgb(48 68 54 / var(--tw-text-opacity));
+        // 600: "#334337",
+        DEFAULT: "#80b990",
+        500: "#80b990", //     color: rgb(128 185 144 / var(--tw-text-opacity));
+        200: "#92e3a9", //     color: rgb(146 227 169 / var(--tw-text-opacity));
+        100: "#b3ebc3", //     color: rgb(179 235 195 / var(--tw-text-opacity));
+        50: "#ebf7eb", //     color: rgb(235 247 235 / var(--tw-text-opacity));
+      },
     },
   },
   plugins: [],
