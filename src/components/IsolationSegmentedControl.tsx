7fe2740f57f91420c5aaa4d6f11dd069d8fdf60e
pom421 <mauguet.po@gmail.com>
2024-06-18 15:19:47 +0200

    feat: wording

--- src/components/IsolationSegmentedControl.tsx
+++ src/components/IsolationSegmentedControl.tsx
@@ -1,16 +1,24 @@
 "use client";
 
 import { SegmentedControl, type SegmentedControlProps } from "@codegouvfr/react-dsfr/SegmentedControl";
+import { cx } from "@codegouvfr/react-dsfr/tools/cx";
 import { useRouter, useSearchParams } from "next/navigation";
 
 import { type TravauxNiveauIsolation } from "@/lib/common/domain/values/TravauxNiveauIsolation";
 import { createSearchParams } from "@/utils/searchParams";
 
+const wordings: Record<TravauxNiveauIsolation, string> = {
+  Global: "En plus du système de chauffage, vous projetez d’isoler la toiture, les murs, le sol et les fenêtres.",
+  Partiel:
+    "En plus du système de chauffage, vous projetez d'avoir une isolation à minima pour la toiture et les fenêtres.",
+  Aucun: "Vous projetez de remplacer uniquement votre système de chauffage.",
+};
+
 export const TravauxNiveauIsolationSegmentedControl = () => {
   const router = useRouter();
   const searchParams = useSearchParams();
 
-  const travauxNiveauIsolation = searchParams.get("travauxNiveauIsolation");
+  const travauxNiveauIsolation = searchParams.get("travauxNiveauIsolation") as TravauxNiveauIsolation | null;
 
   return (
     <>
@@ -38,6 +46,8 @@ export const TravauxNiveauIsolationSegmentedControl = () => {
           })) as unknown as SegmentedControlProps.Segments
         }
       />
+
+      {travauxNiveauIsolation && <p className={cx("mt-4")}>{wordings[travauxNiveauIsolation]}</p>}
     </>
   );
 };
