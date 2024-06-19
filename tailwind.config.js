aa5ab69ebafc576cded65020dfea965a186b9949
pom421 <mauguet.po@gmail.com>
2024-06-19 12:17:24 +0200

    feat: change color of stepper's progress bar

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,14 +21,14 @@ module.exports = {
       black: colors.black,
       white: colors.white,
       green: {
-        900: "var(--color-green-900)", //     color: rgb(24 61 47 / var(--tw-text-opacity));
-        800: "var(--color-green-900)", //     color: rgb(39 68 48 / var(--tw-text-opacity));
-        700: "var(--color-green-700)", //     color: rgb(48 68 54 / var(--tw-text-opacity));
+        900: "var(--color-green-900)",
+        800: "var(--color-green-900)",
+        700: "var(--color-green-700)",
         DEFAULT: "var(--color-green-700)",
-        500: "var(--color-green-500)", //     color: rgb(128 185 144 / var(--tw-text-opacity));
-        200: "var(--color-green-200)", //     color: rgb(146 227 169 / var(--tw-text-opacity));
-        100: "var(--color-green-100)", //     color: rgb(179 235 195 / var(--tw-text-opacity));
-        50: "var(--color-green-50)", //     color: rgb(235 247 235 / var(--tw-text-opacity));
+        500: "var(--color-green-500)",
+        200: "var(--color-green-200)",
+        100: "var(--color-green-100)",
+        50: "var(--color-green-50)",
       },
     },
   },
