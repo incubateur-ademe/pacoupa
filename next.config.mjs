574f2d6c64491c5adddb101b077937c5785d90fa
pom421 <mauguet.po@gmail.com>
2024-06-25 13:07:20 +0200

    feat: show process.env variable at build time

--- next.config.mjs
+++ next.config.mjs
@@ -132,3 +132,7 @@ const withMDX = createMDX({
 });
 
 export default withMDX(config);
+
+console.log("dans Next.config ------------------------");
+console.log("TURSO_DATABASE_URL", process.env.TURSO_DATABASE_URL);
+console.log("TURSO_AUTH_TOKEN", process.env.TURSO_AUTH_TOKEN);
