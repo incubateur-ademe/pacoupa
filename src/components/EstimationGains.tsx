1170b72a907152c7f35ec48ab533b4c5ec11b629
pom421 <mauguet.po@gmail.com>
2024-07-16 12:28:54 +0200

    feat: add disclaimer for costs + approximations

--- src/components/EstimationGains.tsx
+++ src/components/EstimationGains.tsx
@@ -1,4 +1,6 @@
 import { fr } from "@codegouvfr/react-dsfr";
+import { cx } from "@codegouvfr/react-dsfr/tools/cx";
+import { Tooltip } from "@mui/material";
 import assert from "assert";
 import { type PropsWithChildren } from "react";
 
@@ -7,9 +9,11 @@ import { Text } from "@/dsfr/base/typography";
 import { type InformationBatiment } from "@/lib/common/domain/InformationBatiment";
 import { type SolutionAvecEnergieCout } from "@/lib/common/domain/values/SolutionAvecEnergie";
 import { formatEuroNoDecimal } from "@/utils/currency";
+import { approximation } from "@/utils/number";
 
 import { BadgeEuros } from "./BadgeEuros";
 import { BadgePacoupa } from "./BadgePacoupa";
+import { Callout } from "./Callout";
 import { DPEImage } from "./img/DPEImage";
 import { FlecheImage } from "./img/FlecheImage";
 
@@ -17,6 +21,11 @@ type Props = {
   informationBatiment: InformationBatiment;
   solution: SolutionAvecEnergieCout;
 };
+
+const approximation10k = approximation(4);
+const approximation1k = approximation(3);
+const approximation100 = approximation(2);
+const approximation10 = approximation(1);
 
 export const EstimationGains = ({ solution, informationBatiment }: PropsWithChildren<Props>) => {
   assert(solution.cepAvant && solution.cepApres, "cepAvant and cepApres should be defined");
@@ -28,7 +37,15 @@ export const EstimationGains = ({ solution, informationBatiment }: PropsWithChil
 
   const gainEconomique = coutApres - coutAvant;
 
-  console.log({ solution });
+  const approximationEnveloppeImmeuble = approximation10k(
+    (solution.coutIsolationEnveloppe ?? 0) * informationBatiment.nbLogements,
+  );
+  const approximationSystemeImmeuble = approximation10k(
+    (solution.coutInstallationSysteme ?? 0) * informationBatiment.nbLogements,
+  );
+
+  const approximationEnveloppe = approximation1k(solution.coutIsolationEnveloppe ?? 0);
+  const approximationSysteme = approximation1k(solution.coutInstallationSysteme ?? 0);
 
   return (
     <>
@@ -54,24 +71,49 @@ export const EstimationGains = ({ solution, informationBatiment }: PropsWithChil
           </div>
           <p className="mt-2 mb-2 text-sm font-medium">Gains économiques</p>
           <BadgeEuros
-            value={Math.abs(gainEconomique) * informationBatiment.nbLogements}
+            value={Math.abs(approximation100(gainEconomique)) * informationBatiment.nbLogements}
             type="green"
             prefix={gainEconomique < 0 ? "-" : undefined}
           />{" "}
           / mois
-          <p className="text-sm leading-6">{formatEuroNoDecimal(Math.abs(gainEconomique))} / logement</p>
+          <p className="text-sm leading-6">
+            {Math.sign(gainEconomique) === -1 && "- "}
+            {formatEuroNoDecimal(Math.abs(approximation10(gainEconomique)))} / logement
+          </p>
         </div>
         <p className="mb-0">Estimation des coûts</p>
         <p className="text-xs font-normal">(rénovations comprises)</p>
         <div className="px-2">
-          <div className="text-sm font-medium mb-3">Coût total du projet</div>
-          <div
-            title={`coût isolation enveloppe = ${solution.coutIsolationEnveloppe} \ncoût installation systeme = ${solution.coutInstallationSysteme}`}
-          >
-            <BadgeEuros
-              value={(solution.coutIsolationEnveloppe ?? 0) + solution.coutInstallationSysteme}
-              type="sand"
-              prefix="≈"
+          <div className="text-sm font-medium mb-3">
+            Coût total du projet
+            <Tooltip
+              title={
+                <>
+                  <ul>
+                    <li>coût isolation enveloppe : {formatEuroNoDecimal(approximationEnveloppeImmeuble)}</li>
+                    <li>coût installation systeme : {formatEuroNoDecimal(approximationSystemeImmeuble)}</li>
+                  </ul>
+                </>
+              }
+              arrow
+            >
+              <span className={cx(fr.cx("ri-information-line", "fr-icon--sm"), "ml-2")} />
+            </Tooltip>
+          </div>
+          <div>
+            <BadgeEuros value={approximationEnveloppeImmeuble + approximationSystemeImmeuble} type="sand" prefix="≈" />
+            <p className="text-sm leading-6">
+              {formatEuroNoDecimal(approximationEnveloppe + approximationSysteme)} / logement
+            </p>
+
+            <Callout
+              type="warning"
+              content={
+                <>
+                  Les données financières sont des premières estimations. Elles doivent faire l'objet d'études plus
+                  approfondies.
+                </>
+              }
             />
           </div>
           {/* <div className="text-sm font-medium mt-4 mb-3">Aides actionnables</div>
