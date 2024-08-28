75c756067474b6f8957f39beca5d254ca8e484af
pom421 <mauguet.po@gmail.com>
2024-08-28 15:13:44 +0200

    feat: remove dsfr utility classes to tailwind

--- src/app/_landing/raisons-zone.tsx
+++ src/app/_landing/raisons-zone.tsx
@@ -2,7 +2,7 @@ import { HCard } from "@/components/HCard";
 import { ChaudiereCasseeImage } from "@/components/img/ChaudiereCasseeImage";
 import { MonnaieImage } from "@/components/img/MonnaieImage";
 import { SoleilImage } from "@/components/img/SoleilImage";
-import { Container, Grid, GridCol } from "@/dsfr";
+import { Container } from "@/dsfr";
 import { H2 } from "@/dsfr/base/typography";
 
 export const RaisonsZone = () => {
@@ -10,35 +10,31 @@ export const RaisonsZone = () => {
     <Container>
       <H2>On a tous une bonne raison</H2>
 
-      <Grid>
-        <GridCol base={12}>
+      <div className="grid grid-cols-5 gap-4">
+        <div className="col-span-3">
           <HCard
             title="Chaudière en panne&nbsp;?"
             desc="C’est le bon moment pour vous renseigner sur les solutions durables, spécifiquement adaptées à votre immeuble."
             image={<ChaudiereCasseeImage width={100} />}
           />
-        </GridCol>
-      </Grid>
+        </div>
 
-      <Grid className="mt-8">
-        <GridCol base={12} offsetMd={2}>
+        <div className="col-start-2 col-span-3">
           <HCard
             title="Facture trop élevée&nbsp;?"
             desc="Les solutions “renouvelables” sont souvent moins gourmandes, et donc moins exposées aux augmentations de prix."
             image={<MonnaieImage width={100} />}
           />
-        </GridCol>
-      </Grid>
+        </div>
 
-      <Grid className="mt-8">
-        <GridCol base={12} offsetMd={4}>
+        <div className="col-start-3 col-span-3">
           <HCard
             desc="Réseau de chaleur ? pompe à chaleur ? solaire thermique ? biomasse ? Késako ? Les solutions sont nombreuses, laissez nous vous guider pas à pas."
             title="Envie de passer au vert&nbsp;?"
             image={<SoleilImage width={100} />}
           />
-        </GridCol>
-      </Grid>
+        </div>
+      </div>
     </Container>
   );
 };
