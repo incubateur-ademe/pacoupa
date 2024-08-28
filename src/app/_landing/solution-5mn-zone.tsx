eb4330f9c1a2f138274d0ae783812c22795424bd
pom421 <mauguet.po@gmail.com>
2024-08-28 15:13:58 +0200

    feat: improve landing page section

--- src/app/_landing/solution-5mn-zone.tsx
+++ src/app/_landing/solution-5mn-zone.tsx
@@ -11,24 +11,29 @@ export const Solution5mnZone = () => {
       <Container>
         <H2>La solution qui vous correspond en moins de 5min</H2>
 
-        <div className="flex flex-col md:flex-row gap-4 md:gap-32 items-stretch">
-          <VCard
-            title="Simple"
-            desc="Répondez à seulement quelques questions."
-            image={<QuestionnaireImage width={80} />}
-          />
+        <div className="flex flex-col md:flex-row items-center md:items-stretch gap-4 justify-between">
+          <div className="max-w-[280px]">
+            <VCard
+              title="Simple"
+              desc="Répondez à seulement quelques questions."
+              image={<QuestionnaireImage width={80} />}
+            />
+          </div>
 
-          <VCard
-            title="Personnalisée"
-            desc="Déterminez la solution faite pour votre copropriété."
-            image={<CrayonImage width={80} />}
-          />
-
-          <VCard
-            title="Compréhensible"
-            desc="Bénéficiez de conseils d’experts accessibles pour tous."
-            image={<ComprehensibleImage width={80} />}
-          />
+          <div className="max-w-[280px]">
+            <VCard
+              title="Personnalisée"
+              desc="Déterminez la solution faite pour votre copropriété."
+              image={<CrayonImage width={80} />}
+            />
+          </div>
+          <div className="max-w-[280px]">
+            <VCard
+              title="Compréhensible"
+              desc="Bénéficiez de conseils d’experts accessibles pour tous."
+              image={<ComprehensibleImage width={80} />}
+            />
+          </div>
         </div>
       </Container>
     </div>
