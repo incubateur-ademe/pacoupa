cf02a04b882888a383450886610a7bfb7ffcfee0
Pierre-Olivier Mauguet <pierre-olivier.mauguet@beta.gouv.fr>
2024-09-05 16:57:57 +0200

    Fix: pages d'erreurs Next mal positionnées dans le grid (#68)

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,10 +2,10 @@ import { ErrorDisplay } from "@/components/ErrorDisplay";
 import { MatomoPush } from "@/components/utils/MatomoPush";
 
 const NotFound = () => (
-  <>
+  <div className="col-start-2 mt-4">
     <MatomoPush event={["trackEvent", "404", "Page non trouvée"]} />
     <ErrorDisplay code="404" />
-  </>
+  </div>
 );
 
 export default NotFound;
