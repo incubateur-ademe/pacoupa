b6f315511d18baba2f8db34017c746a53de76949
pom421 <mauguet.po@gmail.com>
2024-09-10 11:37:18 +0200

    feat: refactor image with priority

--- src/components/img/HeroImage.tsx
+++ src/components/img/HeroImage.tsx
@@ -3,7 +3,5 @@ import Image from "next/image";
 import hero from "../../../public/img/hero.svg";
 
 export function HeroImage() {
-  return (
-    <Image priority={true} src={hero as string} alt="Immeuble à la loupe" sizes="100vw" className="h-auto w-full" />
-  );
+  return <Image priority src={hero as string} alt="Immeuble à la loupe" sizes="100vw" className="h-auto w-full" />;
 }
