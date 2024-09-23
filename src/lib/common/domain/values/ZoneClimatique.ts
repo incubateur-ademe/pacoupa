2ddc6e963051d191289da6fe62c1c69708164330
pom421 <mauguet.po@gmail.com>
2024-09-23 17:13:42 +0200

    fix: adaptation to data in db

--- src/lib/common/domain/values/ZoneClimatique.ts
+++ src/lib/common/domain/values/ZoneClimatique.ts
@@ -1,3 +1,3 @@
-export const enumZoneClimatique = ["75 - Paris", "34 - Hérault", "36 - Indre"] as const;
+export const enumZoneClimatique = ["75 - Paris", "34 - Herault", "36 - Indre"] as const;
 
 export type ZoneClimatique = (typeof enumZoneClimatique)[number];
