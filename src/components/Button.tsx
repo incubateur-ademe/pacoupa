35a874caede05252ade54f4f5b8c21fc6feb6214
pom421 <mauguet.po@gmail.com>
2024-09-24 16:25:38 +0200

    feat: refactor button in pure tailwind

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,5 +1,6 @@
 import ButtonDsfr from "@codegouvfr/react-dsfr/Button";
 import { cx } from "@codegouvfr/react-dsfr/tools/cx";
+import { type AnchorHTMLAttributes, type ButtonHTMLAttributes, type PropsWithChildren } from "react";
 
 import styles from "./Button.module.scss";
 
@@ -8,7 +9,7 @@ type Props = Parameters<typeof ButtonDsfr>[0];
 /**
  * Custom button for Pacoupa
  */
-export const Button = (props: Props) => {
+export const OldButton = (props: Props) => {
   return (
     <>
       <ButtonDsfr
@@ -25,6 +26,56 @@ export const Button = (props: Props) => {
       >
         {props.children}
       </ButtonDsfr>
+    </>
+  );
+};
+
+type ButtonProps = {
+  priority?: "primary" | "secondary" | "tertiary";
+  size?: "medium" | "small";
+} & (
+  | {
+      buttonProps: ButtonHTMLAttributes<HTMLButtonElement>;
+      linkProps?: never;
+    }
+  | {
+      buttonProps?: never;
+      linkProps: AnchorHTMLAttributes<HTMLAnchorElement>;
+    }
+);
+
+export const Button = (props: PropsWithChildren<ButtonProps>) => {
+  const { priority = "primary", size = "medium" } = props;
+
+  const prioritySize = {
+    medium: "h-[38px] px-6 py-2",
+    small: "h-[30px] px-4 py-1",
+  };
+
+  const priorityStyle = {
+    primary:
+      "!bg-[#92e3a9] hover:!bg-[#b3ebc3] rounded shadow active:!shadow-none active:!bg-[#80b990] active:!border active:!border-[#304436] active:!border-solid",
+    secondary: "bg-white hover:!bg-[#e7f6ec] active:!border active:!border-solid active:!border-[#304436]",
+    tertiary: "border-b-2 border-[#304436] hover:!border-[#80b990] hover:!text-[#80b990] active:!text-[#92e3a9]",
+  };
+
+  return (
+    <>
+      {props.linkProps ? (
+        <a
+          {...props.linkProps}
+          className={`justify-center items-center gap-2 inline-flex text-[#304436] font-bold ${prioritySize[size]} ${priorityStyle[priority]}`}
+        >
+          {props.children}
+        </a>
+      ) : (
+        <button
+          {...props.buttonProps}
+          className={`justify-center items-center gap-2 inline-flex text-[#304436] font-bold ${prioritySize[size]} ${priorityStyle[priority]}`}
+        >
+          {props.children}
+        </button>
+      )}
     </>
   );
 };
