86cab139acc928df87a94a4007c7aebb656fd132
Pierre-Olivier Mauguet <pierre-olivier.mauguet@beta.gouv.fr>
2024-09-25 17:15:47 +0200

    feat: move "Super nouvelle" message on top (#82)

--- src/app/simulation/resultat/Isolation.tsx
+++ src/app/simulation/resultat/Isolation.tsx
@@ -1,6 +1,5 @@
 import { Badge } from "@/components/Badge";
 import { Callout } from "@/components/Callout";
-import { RaisingHands } from "@/components/img/twemoji/RaisingHands";
 import { Text } from "@/dsfr/base/typography";
 import { type GesteIsolation } from "@/lib/common/domain/values/GesteIsolation";
 import { type TravauxNiveauIsolation } from "@/lib/common/domain/values/TravauxNiveauIsolation";
@@ -8,12 +7,11 @@ import { type TravauxNiveauIsolation } from "@/lib/common/domain/values/TravauxN
 import { ShowIsolationImages } from "./ShowIsolationImages";
 
 type Props = {
-  estGlobalementRenove: boolean;
   gestes: GesteIsolation[];
   travauxNiveauIsolation: TravauxNiveauIsolation;
 };
 
-export const Isolation = ({ gestes, travauxNiveauIsolation, estGlobalementRenove }: Props) => {
+export const Isolation = ({ gestes, travauxNiveauIsolation }: Props) => {
   return (
     <>
       <Text className="mb-2">Isolations à prévoir</Text>
@@ -31,23 +29,7 @@ export const Isolation = ({ gestes, travauxNiveauIsolation, estGlobalementRenove
       <div className="mt-4 flex justify-center gap-8">
         <ShowIsolationImages gestes={gestes} />
       </div>
-      {estGlobalementRenove && (
-        <div className="mt-4 flex justify-center gap-8">
-          <Callout
-            type="pacoupa"
-            content={
-              <Text variant="sm" className="leading-6 mb-0">
-                <strong>Super nouvelle !</strong>
-                <br /> Vous habitez dans un immeuble qui est énergiquement déjà très performant ! Il n’y a plus qu’à
-                changer le chauffage&nbsp;
-                <span className="inline-block w-[20px] align-middle">
-                  <RaisingHands />
-                </span>
-              </Text>
-            }
-          />
-        </div>
-      )}
+
       {gestes.length > 0 && (
         <div className="my-4">
           <Callout
