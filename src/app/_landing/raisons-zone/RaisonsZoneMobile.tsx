728c4b758c65992a70941507350087ac33171402
Pierre-Olivier Mauguet <mauguet.po@gmail.com>
2024-10-21 13:23:06 +0200

    feat: add carousel for homepage

--- src/app/_landing/raisons-zone/RaisonsZoneMobile.tsx
+++ src/app/_landing/raisons-zone/RaisonsZoneMobile.tsx
@@ -1,46 +1,33 @@
 "use client";
-import { useEffect, useState } from "react";
+
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "@/components/Carousel";
 
 import { RaisonCard1, RaisonCard2, RaisonCard3 } from "./cards";
 
 export const RaisonsZoneMobile = () => {
-  const [currentIndex, setCurrentIndex] = useState(0);
-  const cards = [RaisonCard1, RaisonCard2, RaisonCard3];
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentIndex(prevIndex => (prevIndex + 1) % cards.length);
-    }, 2000);
-
-    return () => clearInterval(interval);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  const showCard = (index: number) => {
-    setCurrentIndex(index);
-  };
-
-  const CurrentCard = cards[currentIndex];
-
   return (
     <div className="md:hidden relative w-full max-w-2xl mx-auto">
-      <div className="relative w-full h-auto">
-        <CurrentCard />
-      </div>
-
-      <div className="flex justify-center gap-2 mt-6">
-        {cards.map((_, index) => (
-          <button
-            key={index}
-            onClick={() => showCard(index)}
-            className={`w-4 h-4 rounded-full border-2 border-solid border-green-900 ${
-              // we use ! CSS rule because of the behavior of the DSFR button which is white on hover.
-              index === currentIndex ? "bg-body-700 hover:!bg-body-700" : "bg-white hover:!bg-body-300"
-            }`}
-            aria-label={`Aller à la carte ${index + 1}`}
-          />
-        ))}
-      </div>
+      <Carousel>
+        <CarouselContent>
+          <>
+            <CarouselItem>
+              <div className="relative w-[calc(100%-2rem)] h-[300px] mx-auto">
+                <RaisonCard1 />
+              </div>
+            </CarouselItem>
+              <div className="relative w-[calc(100%-2rem)] h-[300px] mx-auto">
+                <RaisonCard2 />
+              </div>
+            </CarouselItem>
+              <div className="relative w-[calc(100%-2rem)] h-[300px] mx-auto">
+                <RaisonCard3 />
+              </div>
+            </CarouselItem>
+          </>
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
     </div>
   );
 };
