880f749f6729e319e3c39684d884bcf71cf94dde
Pierre-Olivier Mauguet <mauguet.po@gmail.com>
2024-11-05 17:37:55 +0100

    fix: remove fiche reference 20 which has no content

--- src/app/simulation/resultat/[idSolution]/FicheReferenceList.tsx
+++ src/app/simulation/resultat/[idSolution]/FicheReferenceList.tsx
@@ -12,6 +12,8 @@ interface FicheReferenceProps {
 export const FicheReferenceList: React.FC<FicheReferenceProps> = ({ solution }) => {
   const fiches = Object.values(fichesReference).filter(value => value.solutionId.startsWith(solution.id));
 
+  if (fiches.length === 0) return null;
+
   return (
     <>
       <H3 className="text-lg font-medium mb-4">Exemple{fiches && fiches.length > 1 ? "s" : ""} d’application</H3>
