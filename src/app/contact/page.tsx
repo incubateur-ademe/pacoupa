bca977e80ece7c98d4985638956e723082cc5d6e
Pierre-Olivier Mauguet <mauguet.po@gmail.com>
2024-11-05 12:13:29 +0100

    feat: add tally in consent management + add Gestion des cookies in the footer

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,26 +1,12 @@
-"use client";
-
 import Link from "next/link";
-import { useEffect, useRef } from "react";
 
+import { ClientOnly } from "@/components/ClientOnly";
 import { config } from "@/config";
 import { H1 } from "@/dsfr/base/typography";
-import { useScrollTop } from "@/lib/client/useScrollTop";
 
-/**
- * This page needs to be a client component because it uses the `useScrollTop` hook and the `iframe` element.
- * The iframe must be fetched client side in order to work in client navigation.
- */
+import { DetailContact } from "./DetailContact";
+
 const ContactPage = () => {
-  useScrollTop();
-  const iframeRef = useRef<HTMLIFrameElement>(null);
-
-  useEffect(() => {
-    if (iframeRef.current) {
-      iframeRef.current.src = `https://tally.so/embed/${config.tally.contact.id}?alignLeft=1&hideTitle=1&transparentBackground=1&dynamicHeight=1`;
-    }
-  }, []);
-
   return (
     <div className="col-start-2 mt-10 md:mt-20 relative ">
       <H1 className="relative md:before:content-[''] before:block before:absolute before:top-[-35px] before:left-[-55px] before:w-full before:h-[91px] before:max-w-[650px] before:bg-[url('/img/dot.png')] before:bg-no-repeat">
@@ -34,9 +20,9 @@ const ContactPage = () => {
         question.
       </p>
 
-      <div className="border border-solid border-body-700 shadow rounded-lg px-16 pb-8">
-        <iframe ref={iframeRef} loading="eager" width="100%" height="100%" title="Page de contact" allowFullScreen />
-      </div>
+      <ClientOnly>
+        <DetailContact />
+      </ClientOnly>
     </div>
   );
 };
