375947060e7777b3a0e37e165acda5451a891885
Pierre-Olivier Mauguet <mauguet.po@gmail.com>
2024-12-02 17:06:44 +0100

    feat: simplify mdx files

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -1,5 +1,5 @@
 import { type MDXComponents } from "mdx/types";
-import { Fragment, type PropsWithChildren } from "react";
+import { Fragment } from "react";
 
 import { MdxLink } from "@/components/mdx/Link";
 import { getLabelFromChildren } from "@/utils/react";
@@ -7,36 +7,9 @@ import { slugify } from "@/utils/string";
 
 import { CTA } from "./components/CTA";
 import { MdxCard } from "./components/MdxCard";
+import { MdxDetails } from "./components/MdxDetails";
+import { MdxSpacer } from "./components/MdxSpacer";
 import { AnchorLink } from "./dsfr/client";
-
-export const Details = ({ children }: PropsWithChildren) => {
-  return (
-    /* using a name for all Details make it like an accordion, with only one item open maximum */
-    <details name="details" className="pl-6 marker:text-decoration-700">
-      {children}
-    </details>
-  );
-};
-
-export const Question = ({ children }: PropsWithChildren) => {
-  return (
-    <AnchorLink
-      as="summary"
-      anchor={slugify(getLabelFromChildren(children))}
-      className="mt-2 list-outside focus:!outline-primary-700"
-    >
-      <b>{children}</b>
-    </AnchorLink>
-  );
-};
-export const Reponse = ({ children }: PropsWithChildren) => {
-  return <div className="pt-2">{children}</div>;
-};
-
-export const Spacer = ({ size }: { size: number }) => {
-  // eslint-disable-next-line react/forbid-dom-props
-  return <div style={{ marginTop: `${size ?? "4"}px` }} />;
-};
 
 export const anchorHeadingMDXComponents: MDXComponents = {
   h1: props => <AnchorLink as="h1" anchor={slugify(getLabelFromChildren(props.children))} {...props} />,
@@ -55,10 +28,8 @@ export const defaultMdxComponents: MDXComponents = {
   a: MdxLink,
   CTA,
   Card: MdxCard,
-  Details,
-  Question,
-  Reponse,
-  Spacer: Spacer,
+  Details: MdxDetails,
+  Spacer: MdxSpacer,
   ...anchorHeadingMDXComponents,
 };
 
