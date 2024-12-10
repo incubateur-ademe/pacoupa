8cdc2ec01bb686c20227c2d364d70d365ddb26d7
Pierre-Olivier Mauguet <mauguet.po@gmail.com>
2024-12-10 12:09:16 +0100

    feat: refactor sitemap and robots file generation

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,15 +2,16 @@ const isDeployment = !!process.env.VERCEL_URL;
 
 const priorities = {
   "/": 1.0,
+  "/simulation": 1.0,
   "/stats": 0.5,
-  "/healthz": 0,
 };
 
 /** @type {import('next-sitemap').IConfig} */
 const config = {
-  generateRobotsTxt: false,
+  generateRobotsTxt: true,
   siteUrl: isDeployment ? `https://${process.env.VERCEL_URL}` : "http://localhost:3000",
   changefreq: "weekly",
+  exclude: ["/debug", "/debug/*", "/healthz", "/blog/playground"],
   transform: async (config, path) => {
     return {
       loc: path,
@@ -19,6 +20,15 @@ const config = {
       alternateRefs: config.alternateRefs ?? [],
       changefreq: config.changefreq,
     };
+  },
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: "*",
+        allow: "/",
+        disallow: ["/debug", "/debug/*", "/healthz", "/blog/playground"],
+      },
+    ],
   },
 };
 
