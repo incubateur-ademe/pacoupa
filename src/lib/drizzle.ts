079deb2eb1a3ded538fbe4ece3c1af7d1c54704a
Lilian Saget-Lethias <lilian.sagetlethias@gmail.com>
2025-05-05 10:43:26 +0200

    chore: switch to libsql-stateless-easy

--- src/lib/drizzle.ts
+++ src/lib/drizzle.ts
@@ -1,10 +1,13 @@
-import { createClient } from "@libsql/client";
 import { drizzle } from "drizzle-orm/libsql";
+import { createClient } from "libsql-stateless-easy";
+
+import { config } from "@/config";
 
 const client = createClient({
   url: process.env.TURSO_DATABASE_URL!,
   authToken: process.env.TURSO_AUTH_TOKEN,
+  fetch,
 });
 
-export const db = drizzle(client, { logger: true });
+export const db = drizzle(client, { logger: config.env !== "prod" });
 // export const db = drizzle(client, { logger: false });
