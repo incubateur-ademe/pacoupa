6d5d7f457820d5045e803f618dd2e72976cff150
Arnaud AMBROSELLI <arnaud@ambroselli.io>
2025-07-07 12:22:25 +0200

    biko font

--- src/app/(coachcopro)/%5F%5Fcoachcopro/page.tsx
+++ src/app/(coachcopro)/%5F%5Fcoachcopro/page.tsx
@@ -37,6 +37,8 @@ export default function Page({ searchParams }: { searchParams: CoachCoproSearchP
     <div
       id="coachcopro"
       className={[inter.className, "relative bg-white/90 size-full inter justify-start items-start flex"].join(" ")}
+      // eslint-disable-next-line
+      style={{ fontFamily: "Biko, Inter, sans-serif" }}
     >
       {step < 4 && (
         <>
